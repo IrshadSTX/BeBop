ff22424664ae2010ae2490368cbaf58ea9c1662a
Irshad <irshadkhaleel.p@gmail.com>
2022-12-27 21:42:41 +0530

    recentsongs1

--- bebop_music/lib/screens/recentSongs.dart
+++ bebop_music/lib/screens/recentSongs.dart
@@ -30,145 +30,137 @@ class _RecentlyPlayedState extends State<RecentlyPlayed> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: double.infinity,
-      width: double.infinity,
-      decoration: const BoxDecoration(
-        gradient: LinearGradient(
-          begin: Alignment.topCenter,
-          end: Alignment.bottomCenter,
-          colors: [Color.fromARGB(255, 0, 0, 0), Color.fromARGB(255, 0, 0, 0)],
-        ),
-      ),
-      child: Scaffold(
-        backgroundColor: Colors.transparent,
-        appBar: AppBar(
-          centerTitle: true,
-          title: const Text(
-            'Recent Songs',
-            style: TextStyle(
-              fontFamily: 'UbuntuCondensed',
-              color: Color.fromARGB(255, 15, 159, 167),
-              fontSize: 26,
-            ),
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text(
+          'Recent Songs',
+          style: TextStyle(
+            color: Colors.white,
           ),
-          backgroundColor: Colors.black,
-          elevation: 0,
         ),
-        body: Padding(
-          padding: const EdgeInsets.only(left: 15, right: 15),
-          child: SingleChildScrollView(
-            child: Column(
-              children: [
-                ValueListenableBuilder(
-                  valueListenable: GetRecentSong.recentSongNotifier,
-                  builder: (BuildContext context, List<SongModel> value,
-                      Widget? child) {
-                    if (value.isEmpty) {
-                      return Padding(
-                        padding: const EdgeInsets.only(top: 200),
-                        child: Center(
-                          child: Column(
-                            children: const [
-                              Text(
-                                'No Song In Recents',
-                                style: TextStyle(
-                                  fontFamily: 'UbuntuCondensed',
-                                  color: Color.fromARGB(255, 255, 255, 255),
-                                  fontSize: 18,
-                                ),
-                              ),
-                            ],
-                          ),
+        backgroundColor: const Color.fromARGB(255, 57, 4, 97),
+        elevation: 0,
+      ),
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        // height: double.infinity,
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Colors.black,
+              Color.fromARGB(255, 5, 3, 69),
+              Colors.black,
+            ],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ),
+        ),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              ValueListenableBuilder(
+                valueListenable: GetRecentSong.recentSongNotifier,
+                builder: (BuildContext context, List<SongModel> value,
+                    Widget? child) {
+                  if (value.isEmpty) {
+                    return const Padding(
+                      padding: EdgeInsets.symmetric(vertical: 200),
+                      child: Text(
+                        'No Song Recently played',
+                        style: TextStyle(
+                          fontFamily: 'poppins',
+                          color: Color.fromARGB(255, 255, 255, 255),
+                          fontSize: 18,
                         ),
-                      );
-                    } else {
-                      recentSong = value.reversed.toSet().toList();
-                      return FutureBuilder<List<SongModel>>(
-                        future: _audioQuery.querySongs(
-                          sortType: null,
-                          orderType: OrderType.ASC_OR_SMALLER,
-                          uriType: UriType.EXTERNAL,
-                          ignoreCase: true,
-                        ),
-                        builder: (context, item) {
-                          if (item.data == null) {
-                            return const Center(
-                              child: CircularProgressIndicator(),
-                            );
-                          }
-                          if (item.data!.isEmpty) {
-                            return const Center(
-                                child: Text(
-                              'No Songs Available',
-                              style: TextStyle(
-                                color: Colors.white,
+                      ),
+                    );
+                  } else {
+                    recentSong = value.reversed.toSet().toList();
+                    return FutureBuilder<List<SongModel>>(
+                      future: _audioQuery.querySongs(
+                        sortType: null,
+                        orderType: OrderType.ASC_OR_SMALLER,
+                        uriType: UriType.EXTERNAL,
+                        ignoreCase: true,
+                      ),
+                      builder: (context, item) {
+                        if (item.data == null) {
+                          return const Center(
+                            child: CircularProgressIndicator(),
+                          );
+                        }
+                        if (item.data!.isEmpty) {
+                          return const Center(
+                              child: Text(
+                            'No Songs Available',
+                            style: TextStyle(
+                              color: Colors.white,
+                            ),
+                          ));
+                        }
+                        return ListView.separated(
+                          physics: const NeverScrollableScrollPhysics(),
+                          shrinkWrap: true,
+                          itemBuilder: (BuildContext context, int index) {
+                            return ListTile(
+                              tileColor: Color.fromARGB(255, 12, 12, 12),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10),
                               ),
-                            ));
-                          }
-                          return ListView.separated(
-                            physics: const NeverScrollableScrollPhysics(),
-                            shrinkWrap: true,
-                            itemBuilder: (BuildContext context, int index) {
-                              return ListTile(
-                                tileColor: Color.fromARGB(255, 12, 12, 12),
-                                shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(10),
-                                ),
-                                minVerticalPadding: 10.0,
-                                contentPadding: const EdgeInsets.all(0),
-                                leading: Padding(
-                                  padding: const EdgeInsets.only(left: 10),
-                                  child: QueryArtworkWidget(
-                                    id: recentSong[index].id,
-                                    type: ArtworkType.AUDIO,
-                                    nullArtworkWidget: const Padding(
-                                      padding: EdgeInsets.fromLTRB(5, 5, 5, 5),
-                                      child: Icon(
-                                        Icons.music_note,
-                                        color: Colors.white,
-                                        size: 33,
-                                      ),
+                              minVerticalPadding: 10.0,
+                              contentPadding: const EdgeInsets.all(0),
+                              leading: Padding(
+                                padding: const EdgeInsets.only(left: 10),
+                                child: QueryArtworkWidget(
+                                  id: recentSong[index].id,
+                                  type: ArtworkType.AUDIO,
+                                  nullArtworkWidget: const Padding(
+                                    padding: EdgeInsets.fromLTRB(5, 5, 5, 5),
+                                    child: Icon(
+                                      Icons.music_note,
+                                      color: Colors.white,
+                                      size: 33,
                                     ),
                                   ),
                                 ),
-                                title: Text(
-                                  recentSong[index].displayNameWOExt,
-                                  maxLines: 1,
-                                ),
-                                subtitle: Text(
-                                  '${recentSong[index].artist == "<unknown>" ? "Unknown Artist" : recentSong[index].artist}',
-                                  maxLines: 1,
-                                ),
-                                onTap: () {
-                                  GetAllSongController.audioPlayer
-                                      .setAudioSource(
-                                          GetAllSongController.createSongList(
-                                              recentSong),
-                                          initialIndex: index);
-                                  GetAllSongController.audioPlayer.play();
-                                  Navigator.push(context,
-                                      MaterialPageRoute(builder: (context) {
-                                    return PlayerScreen(
-                                        songModelList: recentSong);
-                                  }));
-                                },
-                              );
-                            },
-                            itemCount: recentSong.length,
-                            separatorBuilder: (context, index) {
-                              return const Divider(
-                                height: 10.0,
-                              );
-                            },
-                          );
-                        },
-                      );
-                    }
-                  },
-                )
-              ],
-            ),
+                              ),
+                              title: Text(
+                                recentSong[index].displayNameWOExt,
+                                maxLines: 1,
+                              ),
+                              subtitle: Text(
+                                '${recentSong[index].artist == "<unknown>" ? "Unknown Artist" : recentSong[index].artist}',
+                                maxLines: 1,
+                              ),
+                              onTap: () {
+                                GetAllSongController.audioPlayer.setAudioSource(
+                                    GetAllSongController.createSongList(
+                                        recentSong),
+                                    initialIndex: index);
+                                GetAllSongController.audioPlayer.play();
+                                Navigator.push(context,
+                                    MaterialPageRoute(builder: (context) {
+                                  return PlayerScreen(
+                                      songModelList: recentSong);
+                                }));
+                              },
+                            );
+                          },
+                          itemCount: recentSong.length,
+                          separatorBuilder: (context, index) {
+                            return const Divider(
+                              height: 10.0,
+                            );
+                          },
+                        );
+                      },
+                    );
+                  }
+                },
+              )
+            ],
           ),
         ),
       ),
